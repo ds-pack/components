5502c0fbd7a82d88523b7d2785ee70e51165f0ad
Matt Hamlin <matthewjameshamlin@gmail.com>
2020-05-16 17:54:32 -0400

    Exports tests updated

--- src/__tests__/exports.tsx
+++ src/__tests__/exports.tsx
@@ -3,3 +3,15 @@ import * as components from '../index'
 test('Box is defined', () => {
   expect(components.Box).toBeDefined()
 })
+
+test('useTheme is defined', () => {
+  expect(components.useTheme).toBeDefined()
+})
+
+test('theme is defined', () => {
+  expect(components.theme).toBeDefined()
+})
+
+test('ThemeProvider is defined', () => {
+  expect(components.ThemeProvider).toBeDefined()
+})
