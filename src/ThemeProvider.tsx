1252b6b2be5d79b8935407274e9088aa2cd57422
Matt Hamlin <matthewjameshamlin@gmail.com>
2021-01-20 16:35:32 -0800

    Don't use Box in ThemeProvider

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,33 +1,21 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import defaultTheme from './theme'
 import {
   ThemeProvider as StyledProvider,
   DefaultTheme,
 } from 'styled-components'
-import themeToCSSVars from '@ds-pack/theme-to-css-vars'
-import { Box, Props as BoxProps } from './Box'
 
 export let themeContext = React.createContext(defaultTheme)
 
-interface Props extends BoxProps {
+interface Props {
   theme?: DefaultTheme
   children: React.ReactNode
 }
 
-export function ThemeProvider({ theme = defaultTheme, ...props }: Props) {
-  let vars = useMemo(() => {
-    let cssVars = themeToCSSVars(theme)
-    return cssVars.reduce(
-      (style, [varName, val]) => ({ ...style, [varName]: val }),
-      {},
-    )
-  }, [theme])
-
+export function ThemeProvider({ theme = defaultTheme, children }: Props) {
   return (
     <themeContext.Provider value={theme}>
-      <StyledProvider theme={theme}>
-        <Box {...props} style={vars} />
-      </StyledProvider>
+      <StyledProvider theme={theme}>{children}</StyledProvider>
     </themeContext.Provider>
   )
 }
