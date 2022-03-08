9a83cd4701932ccfa14edfae144c67d11323eaf9
Matt Hamlin <matthewjameshamlin@gmail.com>
2022-03-07 20:01:07 -0500

    Fix react usage

--- packages/experimental-components/scripts/build.js
+++ packages/experimental-components/scripts/build.js
@@ -1,12 +1,36 @@
 let { vanillaExtractPlugin } = require('@vanilla-extract/esbuild-plugin')
 
+let jsxPluginReact17 = {
+  name: 'jsx-react-17',
+  setup(build) {
+    let fs = require('fs')
+    let babel = require('@babel/core')
+    let plugin = require('@babel/plugin-transform-react-jsx').default(
+      {},
+      { runtime: 'automatic' },
+    )
+    let ts = require('@babel/plugin-syntax-typescript').default
+
+    build.onLoad({ filter: /\.tsx$/ }, async (args) => {
+      let jsx = await fs.promises.readFile(args.path, 'utf8')
+      let result = babel.transformSync(jsx, {
+        filename: 'foo.tsx',
+        plugins: [plugin, ts],
+      })
+      return { contents: result.code }
+    })
+  },
+}
+
 require('esbuild')
   .build({
     entryPoints: ['src/index.ts'],
     bundle: true,
     format: 'cjs',
-    plugins: [vanillaExtractPlugin()],
+    plugins: [vanillaExtractPlugin(), jsxPluginReact17],
     outfile: 'compiled.js',
+    // outdir: './dist',
+    // splitting: true,
     external: [
       'react',
       'scheduler',
