8d58fb1908020e853a58389d80199eef8a90be72
Matt Hamlin <matthewjameshamlin@gmail.com>
2022-12-23 07:47:26 -0800

    Babel config, ts config, lets-go

--- packages/components/rollup.config.mjs
+++ packages/components/rollup.config.mjs
@@ -16,14 +16,12 @@ export default {
     preserveModules: true,
     // banner: "'use client';",
     entryFileNames({ name }) {
-      console.log(`Entry: ${name}`)
       if (name) {
         return name.replace('.css', '.vanilla.js')
       }
       return ''
     },
     assetFileNames({ name }) {
-      console.log(`Asset: ${name}`)
       if (name) {
         if (name.endsWith('.css.ts.vanilla.css')) {
           return name.replace('.css.ts.vanilla.css', '.css').replace('src/', '')
